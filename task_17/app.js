58c7fd85a61bb3362d130b0cf7ba09412223f2ef
ZINO\46fir <46firstmate@gmail.com>
2017-08-22 06:34:36 +0900

    solve task_17

--- task_17/app.js
+++ task_17/app.js
@@ -10,8 +10,37 @@ while(true) {
 		userInput = prompt("Enter a new todo");
 		todoList.push(userInput);
 	} else if (userInput === "list") {
-		console.log(todoList);
+		todoList();
 	} else if (userInput === "quit") {
 		break;
+	} else if (userInput === "delete") {
+		deleteTodo();
 	}
+}
+
+console.log("You quit App!");
+
+function todoList() {
+	//**********
+	// index : todo
+	//**********
+	// use For Loop
+	// console.log("*************");
+	// for(var i = 0; i < todoList.length; i++) {
+	// 	console.log(i + " : " + todoList[i]);
+	// }
+	// console.log("*************");
+
+	// use forEach
+	console.log("*************");
+	todoList.forEach(function(todo, index){
+		console.log(index + " : " + todo);
+	});
+	console.log("*************");
+}
+
+function deleteTodo() {
+	userInput = prompt("Enter specific index of todo");
+	todoList.splice(userInput, 1);
+	console.log("Todo Removed");
 }
